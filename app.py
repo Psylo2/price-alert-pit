97e1e265b417e79a8fa5ea2b6254f6fcde125caa
Psylo2 <psylo20@hotmail.com>
2022-02-08 22:03:25 +0200

    update app.py

--- app.py
+++ app.py
@@ -1,29 +1,41 @@
 import os
-from flask import Flask, render_template
+from flask import Flask
 from dotenv import load_dotenv
 
 load_dotenv(".env", verbose=True)
 
-from db.db import Database
-from views.alerts import alert_blueprint
-from views.stores import store_blueprint
-from views.users import user_blueprint
+from manager import Repository, AppConfiguration
+repository = Repository()
+
+from handlers import UserHandler, AlertHandler, StoreHandler, ItemHandler
+from views import home_blueprint, alert_blueprint, user_blueprint, store_blueprint, item_blueprint
 
 app = Flask(__name__)
 
-app.secret_key = os.environ.get('APP_SECRET_KEY')
-app.config.update(ADMIN=os.environ.get('ADMIN'))
+AppConfiguration(app=app)
 
 @app.before_first_request
 def init_db():
-    Database.initialize()
+    repository.initialize()
 
 
-@app.get('/')
-def home():
-    return render_template('home.html')
+# handlers
+user_handler = UserHandler(repository=repository)
+store_handler = StoreHandler(repository=repository)
+item_handler = ItemHandler(repository=repository)
+alert_handler = AlertHandler(repository=repository,
+                             store_handler=store_handler,
+                             item_handler=item_handler)
 
+# inject handlers to blueprints
+user_blueprint.handler = user_handler
+store_blueprint.handler = store_handler
+alert_blueprint.handler = alert_handler
+item_blueprint.handler = item_handler
 
+app.register_blueprint(home_blueprint)
+app.register_blueprint(item_blueprint,
+                       url_prefix="/items")
 app.register_blueprint(alert_blueprint,
                        url_prefix="/alerts")
 app.register_blueprint(store_blueprint,
