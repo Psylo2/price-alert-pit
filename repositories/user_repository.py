f79c547d82db227bd11f2fe5ea90cae2f5645478
Psylo2 <psylo20@hotmail.com>
2022-02-08 21:58:39 +0200

    add models repository

---
+++ repositories/user_repository.py
@@ -0,0 +1,38 @@
+from typing import Dict, List
+
+from app import repository
+from manager.services import RepositoryServices
+
+
+class UserRepository(RepositoryServices):
+
+    def __init__(self):
+        self._repository_instance = None
+        self._repository_name = "user"
+
+    @property
+    def repository(self):
+        if not self._repository_instance:
+            self._repository_instance = repository.create_collection(self._repository_name)
+        return self._repository_instance
+
+    def insert(self, data: dict) -> None:
+        self.repository.insert_one(document=data)
+
+    def find(self, query: dict) -> Dict:
+        return self.repository.find(query)
+
+    def find_one(self, _id: str) -> Dict:
+        filter_ = {"_id": _id}
+        return self.repository.find_one(filter=filter_)
+
+    def update(self, _id: str, data: dict) -> None:
+        filter_ = {"_id": _id}
+        self.repository.update_one(filter=filter_, update=data, upsert=True)
+
+    def remove(self, _id: str) -> Dict:
+        filter_ = {"_id": _id}
+        return self.repository.delete_one(filter=filter_)
+
+    def fetch_all(self) -> List:
+        return self.repository.find({})
